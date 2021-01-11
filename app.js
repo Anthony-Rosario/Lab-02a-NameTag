3fab12a16d4a747288e2fa99d2ae1fa6c7e6aa00
Anthony Rosario <anthonymrosario225@gmail.com>
2021-01-10 22:45:59 -0800

    spruced up tag: added counter and styling, added pronoun button/pronoun font change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const userInput = document.getElementById('user-input');
 const input1 = document.getElementById('user-name-input');
 const inputButton = document.getElementById('input-button');
-const myButton = document.getElementById('button');
+const counterBox = document.getElementById('counter-box');
 const myColor = document.getElementById('nametag');
 const fontChange = document.getElementById('fontC');
 const fontButton = document.getElementById('button3')
@@ -10,26 +10,30 @@ const myColor2 = document.getElementById('nametag');
 const userPronouns = document.getElementById('pronouns');
 const pronounInput = document.getElementById('pronoun-input');
 
+
 let counter = 0;
-let lastName = '';
-
-let prevF = false;
-const basicF = input1.style.fontFamily;
 
 
-//Pronoun Input
 inputButton.addEventListener('click', () => {
-    pronounInput.textContent = userPronouns.value;
+    console.log(input1.innerHTML);
+    const newName = userInput.value;
+    let oldName = input1.innerHTML;
+    if(newName !== oldName){
+        counter ++;
+        counterBox.textContent = counter;
+        input1.textContent = newName;
+        
+    }
+});
     
-})
+
+
+
 
 //User text Input/Pronoun Text
 inputButton.addEventListener('click', () => {
     input1.textContent = userInput.value;
-    
-    pronounInput.textContent = userPronouns.value;
-    
-    
+    userPronouns.textContent = pronounInput.value;
 });
 
 
@@ -45,31 +49,19 @@ myColor.addEventListener('click', generator);
 
 
 //Change Font button
+let prevF = false;
+const basicF = input1.style.fontFamily;
+const basicF2 = userPronouns.style.fontFamily;
 fontButton.addEventListener('click', () => {
-    counter ++;
     if(!prevF){
         input1.style.fontFamily = 'true_liesregular';
-        userPronouns.fontFamily = 'true_liesregular';
+        userPronouns.style.fontFamily = 'true_liesregular';
         prevF = true;
     }
     else{
         input1.style.fontFamily = basicF;
-        userPronouns.style.fontFamily = basicF;
+        userPronouns.style.fontFamily = basicF2;
         prevF = false;
     }
-
 });
-
-
-// //Change BackgroundColor to Aqua
-// myButton.addEventListener('click', () => {
-//     myColor.style.backgroundColor = 'aqua';
-// });
-
-
-// //Change Background Color to Green
-// myButton2.addEventListener('click', () => {
-//     myColor2.style.backgroundColor = 'green';
-
-// });
 
